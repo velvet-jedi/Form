a26878675394d2a0290ede89f52f0a1926aa62a3
velvet-jedi <pranjalruhela2811@gmail.com>
2024-03-04 18:07:12 +0530

    basic design

---
+++ README.md
@@ -0,0 +1,7 @@
+# Form
+
+[How does it look?]()
+
+- Client Side validation of form data
+- Faster validation
+- More control over styling of errors and messages
---
+++ script.js
@@ -0,0 +1,13 @@
+const form = document.getElementById("form");
+
+const name = document.getElementById("name");
+const nameError = document.querySelector("#name + span.error");
+
+const email = document.getElementById("email");
+const emailError = document.querySelector("#email + span.error");
+
+const country = document.getElementById("country");
+const countryError = document.querySelector("#country + span.error");
+
+const zipCode = document.getElementById("zip-code");
+const zipCodeError = document.querySelector("#zip-code + span.error");
